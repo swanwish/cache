caf9cf16a1056340ab82fca7e1037d27c4cf4709
Stephen Lee <liwp.stephen@gmail.com>
2015-04-07 09:51:19 +0800

    add parameter to save db name.

--- cache/cache_leveldb.go
+++ cache/cache_leveldb.go
@@ -16,9 +16,10 @@ type CacheLevelDB struct {
 	LevelDbFileName string
 }
 
-func GetLevelDB(dbFileName string) (*leveldb.DB, error) {
-	if dbFileName == "" {
-		dbFileName = DefaultLevelDbFileName
+func (cache CacheLevelDB) GetLevelDB() (*leveldb.DB, error) {
+	dbFileName := DefaultLevelDbFileName
+	if cache.LevelDbFileName != "" {
+		dbFileName = cache.LevelDbFileName
 	}
 	return leveldb.OpenFile(dbFileName, nil)
 }
@@ -28,7 +29,7 @@ func (cache CacheLevelDB) SetValue(key, value []byte) error {
 		logs.Error("The key is empty.")
 		return ErrParameterMissing
 	}
-	db, err := GetLevelDB()
+	db, err := cache.GetLevelDB()
 	if err != nil {
 		logs.Errorf("Faieled to get level db connection, the error is %v\n", err)
 		return err
@@ -39,7 +40,7 @@ func (cache CacheLevelDB) SetValue(key, value []byte) error {
 }
 
 func (cache CacheLevelDB) GetValue(key []byte) ([]byte, error) {
-	db, err := GetLevelDB()
+	db, err := cache.GetLevelDB()
 	if err != nil {
 		logs.Errorf("Failed to get level db connection, the error is %v\n", err)
 		return []byte{}, err
