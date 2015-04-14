f9f971222bc7469710ec8ffcb6c28e3814b8b644
Stephen Lee <liwp.stephen@gmail.com>
2015-04-14 11:33:55 +0800

    Add get local ip address function.
    Add tool to get local ip addresses.

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
-package helper
+package main
+
+import (
+	"log"
+
+	"github.com/swanwish/go-helper/utils"
+)
 
 func main() {
-
+	localIps, err := utils.GetLocalIPAddrs()
+	if err != nil {
+		log.Printf("Failed to get local ip address, the error is %v\n", err)
+		return
+	}
+	for _, ip := range localIps {
+		log.Printf("IP Address: %s\n", ip)
+	}
 }
